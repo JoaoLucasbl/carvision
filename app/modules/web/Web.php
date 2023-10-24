1e41223285c182b57b5f59d106864805816ea130
Tiago <tcsousa1008@gmail.com>
2023-10-24 16:47:45 -0300

    feat: create Web module

---
+++ app/modules/web/Web.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Modules\Web;
+
+class Web
+{
+    public function home($data){
+        echo "<h1>Olá mundo!</h1>";
+        var_dump($data);
+    }
+
+    public function error($data){
+        echo "<h1>{$data['errorCode']}</h1>";
+        var_dump($data);
+    }
+}
