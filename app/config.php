db6d195c2a3dc0f6994d8a58995325ee7ec15230
Tiago <tcsousa1008@gmail.com>
2023-10-25 20:50:56 -0300

    feat: create global const variables related to the config of the project

--- app/config.php
+++ app/config.php
@@ -1,3 +1,9 @@
 <?php
 
 define("URL_BASE", "http://localhost:8080");
+
+define("DATABASE_HOST", "carvision-db-1");
+define("DATABASE_NAME", "carvision_local");
+define("DATABASE_USERNAME", "root");
+define("DATABASE_PASSWORD", "root");
+define("DATABASE_PORT", "3306");
--- app/index.php
+++ app/index.php
@@ -8,6 +8,7 @@
 //     exit();
 // }
 
+require __DIR__."/config.php";
 require __DIR__."/vendor/autoload.php";
 require __DIR__."/base/TemplateConfiguration.php";
 require __DIR__."/base/Router.php";
