9f7cabb6375015f5332c132ec6bcbfe915b6ae2c
Tiago <tcsousa1008@gmail.com>
2023-10-25 17:51:16 -0300

    feat: isolate index.php / create

--- app/index.php
+++ app/index.php
@@ -9,21 +9,5 @@
 // }
 
 require __DIR__."/vendor/autoload.php";
-
-use CoffeeCode\Router\Router;
-
-$router = new Router("http://localhost:8080"); 
-
-$router->namespace("App\Modules");
-
-$router->group(null);
-$router->get("/", "Web:home");
-
-$router->group("ooops");
-$router->get("/{errorCode}", "Web:error");
-
-$router->dispatch();
-
-if($router->error()){
-    $router->redirect("ooops/".$router->error());
-}
+require __DIR__."/base/TemplateConfiguration.php";
+require __DIR__."/base/Router.php";
